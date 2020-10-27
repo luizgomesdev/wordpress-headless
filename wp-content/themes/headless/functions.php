02ad275286e9ea95e0ae0fc122fdad4b230ba4ac
Luiz Gomes <luizgomesdesenvolvimento@gmail.com>
2020-10-27 17:39:20 -0300

    Add theme suporte

--- wp-content/themes/headless/functions.php
+++ wp-content/themes/headless/functions.php
@@ -1,13 +1,3 @@
 <?php
 
-function restrict_rest_api_to_localhost()
-{
-
-    $whitelist = ['127.0.0.1', "::1", '170.79.54.73'];
-
-    if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
-        die('REST API is disabled for you thanks.');
-    }
-}
-
-add_action('rest_api_init', 'restrict_rest_api_to_localhost', 0);
+require_once "inc/index.php";
